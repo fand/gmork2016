10093d7423caed93a21ddf5d219c6730180ae1a7
amagitakayosi <fand.gmork@gmail.com>
2016-12-27 20:50:37 +0900

    Remove randoms

--- src/js/IndexPage.js
+++ src/js/IndexPage.js
@@ -7,11 +7,7 @@ const WORDS = 'Yes No True False Fake Nothing Evil Innocent'.split(' ');
 class IndexPage extends React.Component {
 
   render () {
-    const space = (x) => _.repeat('&nbsp;', Math.floor((1 - Math.random() ** 3) * x));
-    const body = space(50) + _.range(10).map(() =>
-    _.sample(WORDS) + '.' + space(2000)
-    ).join('');
-
+    const body = _.sample(WORDS);
     return (
       <div className="IndexPage">
         <Helmet title="Gmork.in"/>
